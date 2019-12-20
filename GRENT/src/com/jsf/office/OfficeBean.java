c1d3b6e0e70bf809bf82b347a8060c48f7d35c0f
BoraArseven <bora.arseven@gmail.com>
2019-12-20 19:03:38 +0300

    Office classına filter fonksiyonu eklendi.

--- GRENT/src/com/jsf/office/OfficeBean.java
+++ GRENT/src/com/jsf/office/OfficeBean.java
@@ -9,30 +9,24 @@ import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
 import com.jsf.database.DatabaseManager;
 import com.jsf.entity.Office;
 
-
 @ManagedBean
 @ViewScoped
-public class OfficeBean implements Serializable{
+public class OfficeBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private ArrayList<String> nameSelections;
-	private ArrayList<String> classSelections;
-	private ArrayList<String> vehicleTypeSelections;
-	private ArrayList<String> gearTypeSelections; 
-	private ArrayList<String> fuelTypeSelections;
-	
-	private String selectedPriceRange;
+
 	private ArrayList<String> selectedNames;
 	private ArrayList<String> selectedCities;
 	private ArrayList<String> countries;
 	private ArrayList<String> selectedcountry;
-	private ArrayList<String> selectedGearTypes;
+
 	private ArrayList<String> cities;
-	private ArrayList<String> selectedFuelTypes;
+
 	private ArrayList<Office> Offices;
+
 	public ArrayList<String> getCities() {
 		return cities;
 	}
@@ -42,332 +36,181 @@ public class OfficeBean implements Serializable{
 	}
 
 	private ArrayList<Office> selectedOffices;
-	private int officeCount;       
+	private int officeCount;
 	private Connection connection;
-	
+
 	@PostConstruct
 	public void init() {
 		DatabaseManager.initiliaze();
 		connection = DatabaseManager.getConnection();
-		selectedcountry= new ArrayList<String>();
+		selectedcountry = new ArrayList<String>();
 		countries = new ArrayList<String>();
 		selectedNames = new ArrayList<String>(); // prepare the attributes
 		selectedCities = new ArrayList<String>();
-		
+
 		cities = new ArrayList<String>();
-	
-		selectedFuelTypes = new ArrayList<String>();
+
 		Offices = new ArrayList<Office>();
-		selectedOffices = Offices;
-		nameSelections = new ArrayList<String>(); 
-		
-		classSelections = new ArrayList<String>(); 
-		vehicleTypeSelections = new ArrayList<String>(); 
-		gearTypeSelections = new ArrayList<String>(); 
-		fuelTypeSelections = new ArrayList<String>(); 
+
+		nameSelections = new ArrayList<String>();
+
 		receiveOffices(); // prepare the vehicles
-		receiveCountries(); //select countries in that are in the database
+		receiveCountries(); // select countries in that are in the database
 		receiveCities();
 		receiveNames();
 		officeCount = Offices.size();
-		
-		
+		selectedOffices = Offices;
+
 	}
-	
+
 	public void receiveOffices() {
 		try {
-			PreparedStatement pstmt = connection.prepareStatement(
-				        "SELECT * FROM office ");
-	        ResultSet resultSet1 = pstmt.executeQuery();
+			PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM office ");
+			ResultSet resultSet1 = pstmt.executeQuery();
 			resultSet1.beforeFirst();
-	        while (resultSet1.next()) {
-	        	String name = resultSet1.getString("name");
-	        	String isDeleted = resultSet1.getString("isDeleted");
-	        	String email = resultSet1.getString("email");
-	        	String address = resultSet1.getString("address");
-	        	String phone = resultSet1.getString("phone");
-	        	String fax = resultSet1.getString("fax");
-	        	String workingDays = resultSet1.getString("workingDays");
-	        	String workingHours = resultSet1.getString("workingHours");
-	        	String city = resultSet1.getString("city");
-	        	String country  = resultSet1.getString("country");
-	            Office office = new Office(name, isDeleted, email, address,phone, fax, workingDays, workingHours, city, country);
-	            Offices.add(office);
-	        }
-
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			while (resultSet1.next()) {
+				String name = resultSet1.getString("name");
+				String isDeleted = resultSet1.getString("isDeleted");
+				String email = resultSet1.getString("email");
+				String address = resultSet1.getString("address");
+				String phone = resultSet1.getString("phone");
+				String fax = resultSet1.getString("fax");
+				String workingDays = resultSet1.getString("workingDays");
+				String workingHours = resultSet1.getString("workingHours");
+				String city = resultSet1.getString("city");
+				String country = resultSet1.getString("country");
+				Office office = new Office(name, isDeleted, email, address, phone, fax, workingDays, workingHours, city,
+						country);
+				Offices.add(office);
 			}
-		
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
 	public void receiveCountries() {
 		try {
-			PreparedStatement pstmt = connection.prepareStatement(
-			        "SELECT DISTINCT country FROM office");
-	    ResultSet resultSet1 = pstmt.executeQuery();
-	    
-	   
-		resultSet1.beforeFirst();
-	    while (resultSet1.next()) {
-	    	String countryname = resultSet1.getString("country");
-	    	countries.add(countryname);
-		}} catch (SQLException e) {
+			PreparedStatement pstmt = connection.prepareStatement("SELECT DISTINCT country FROM office");
+			ResultSet resultSet1 = pstmt.executeQuery();
+
+			resultSet1.beforeFirst();
+			while (resultSet1.next()) {
+				String countryname = resultSet1.getString("country");
+				countries.add(countryname);
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	public void receiveCities() {
 		try {
-			PreparedStatement pstmt = connection.prepareStatement(
-			        "SELECT DISTINCT city FROM office");
-	    ResultSet resultSet1 = pstmt.executeQuery();
-	    
-	  
-		resultSet1.beforeFirst();
-	    while (resultSet1.next()) {
-	    	String cityname = resultSet1.getString("city");
-	    	cities.add(cityname);
-		}} catch (SQLException e) {
+			PreparedStatement pstmt = connection.prepareStatement("SELECT DISTINCT city FROM office");
+			ResultSet resultSet1 = pstmt.executeQuery();
+
+			resultSet1.beforeFirst();
+			while (resultSet1.next()) {
+				String cityname = resultSet1.getString("city");
+				cities.add(cityname);
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	public void receiveNames() {
 		try {
-			PreparedStatement pstmt = connection.prepareStatement(
-			        "SELECT DISTINCT name FROM office");
-	    ResultSet resultSet1 = pstmt.executeQuery();
-	    
-	  
-		resultSet1.beforeFirst();
-	    while (resultSet1.next()) {
-	    	String name = resultSet1.getString("name");
-	    	nameSelections.add(name);
-		}} catch (SQLException e) {
+			PreparedStatement pstmt = connection.prepareStatement("SELECT DISTINCT name FROM office");
+			ResultSet resultSet1 = pstmt.executeQuery();
+
+			resultSet1.beforeFirst();
+			while (resultSet1.next()) {
+				String name = resultSet1.getString("name");
+				nameSelections.add(name);
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	public void filter() { // filter action
 		System.out.println("filter");
 		boolean c = checkForFilter();
-		
+
 		if (c == false) {
 			selectedOffices = Offices;
-		}else {
+		} else {
 			selectedOffices = new ArrayList<Office>();
-			
+
 			for (int i = 0; i < Offices.size(); i++) {
 				Office v = Offices.get(i);
-				
+
 				boolean control = true;
-			/*	if(selectedPriceRange != null && !selectedPriceRange.equals("")) {
-					if(!checkPriceRange(String.valueOf(v.getDailyprice()))) {
+
+				if (!selectedCities.isEmpty()) {
+					if (!checkCities(v.getCity())) {
 						control = false;
 					}
 				}
-			if(!classSelections.isEmpty()) {
-					if(!checkClasses(v.getVehicleClass())) {
+
+				if (!selectedNames.isEmpty()) {
+					if (!checkname(v.getName())) {
 						control = false;
 					}
 				}
-				if(!fuelTypeSelections.isEmpty()) {
-					if(!checkFuelTypes(v.getFuelType())) {
+				if (!selectedcountry.isEmpty()) {
+					if (!checkCountry(v.getCountry())) {
 						control = false;
 					}
 				}
-				if(!nameSelections.isEmpty()) {
-					if(!checkNames(v.getName())) {
-						control = false;
-					}
-				}
-				if(!vehicleTypeSelections.isEmpty()) {
-					if(!checkVehicleTypes(v.getType())) {
-						control = false;
-					}
-				}
-				if(!gearTypeSelections.isEmpty()) {
-					if(!checkGearTypes(v.getGearType())) {
-						control = false;
-					}
-				}
-				*/
-				if(control == true) {
+				if (control == true) {
 					selectedOffices.add(v);
 				}
 			}
 		}
-		
-		
-		officeCount = selectedOffices.size();
-		
-	}
-	
-	
-	public boolean checkPriceRange(String price) {
-		
-		int count = 0;
-		for (int i = 0; i < selectedPriceRange.length(); i++) {
-			String check = selectedPriceRange.charAt(i) + "";
-			if(check.equals("-")) {
-				count = i;
-			}
-		}
 
-		int iprice = Integer.parseInt(price);
-		
-		if(count==0) {
-			int temp = Integer.parseInt(selectedPriceRange);
-			if(iprice >= temp) {
-				return true;
-			}
-		}
-		
-		String selected1 = selectedPriceRange.substring(0,count);
-		String selected2 = selectedPriceRange.substring(count+1, selectedPriceRange.length());
-		
-		int iselected1 = Integer.parseInt(selected1);
-		int iselected2 = Integer.parseInt(selected2);
-		
-	
-		if(iprice >= iselected1 && iprice <= iselected2) {
+		officeCount = selectedOffices.size();
+
+	}
+
+	public boolean checkCities(String OfficeCity) {
+		if (selectedCities.contains(OfficeCity)) {
 			return true;
-		}else
+		} else
 			return false;
 	}
-	
-	
-	public boolean checkClasses(String vehicleClass) {
-		if(classSelections.contains(vehicleClass)) {
+
+	public boolean checkname(String name) {
+		if (selectedNames.contains(name)) {
 			return true;
-		}else
+		} else
 			return false;
 	}
-	
-	public boolean checkFuelTypes(String fuelType) {
-		if(fuelTypeSelections.contains(fuelType)) {
+
+	public boolean checkCountry(String country) {
+		if (selectedcountry.contains(country)) {
 			return true;
-		}else
+		} else
 			return false;
 	}
-	
-	public boolean checkNames(String name) {
-		if(nameSelections.contains(name)) {
-			return true;
-		}else 
-			return false;
-	}
-	
-	public boolean checkVehicleTypes(String vehicleType) {
-		if(vehicleTypeSelections.contains(vehicleType)) {
-			return true;
-		}else
-			return false;
-	}
-	
-	public boolean checkGearTypes(String gearType) {
-		if(gearTypeSelections.contains(gearType)) {
-			return true;
-		}else
-			return false;
-	}
-	
+
 	public boolean checkForFilter() {
-		if(selectedPriceRange != null) {
+
+		if (!selectedCities.isEmpty()) {
 			return true;
-		}
-		else if(!classSelections.isEmpty()) {
+		} else if (!selectedNames.isEmpty()) {
 			return true;
-		}else if (!fuelTypeSelections.isEmpty()) {
+		} else if (!selectedcountry.isEmpty()) {
 			return true;
-		}else if (!nameSelections.isEmpty()) {
-			return true;
-		}else if (!vehicleTypeSelections.isEmpty()) {
-			return true;
-		}else if (!gearTypeSelections.isEmpty()) {
-			return true;
-		}else
+		} else
 			return false; // there is no filter selection
 	}
-	/*
-	public void setFilters() { // find the existing filter options for the filter panel
-		filterNames();
-		filterClasses();
-	    filterFuelType();
-	    filterGearType();
-	    filterVehicleTypes();
-	}
-
-	 public void filterNames() {
-		for (int i = 0; i < vehicles.size(); i++) {
-			boolean c = false;
-			for (int j = 0; j < selectedNames.size(); j++) {
-				if(vehicles.get(i).getName().equals(selectedNames.get(j))) {
-					c = true;
-				}
-			}
-			if (c == false ) {
-				selectedNames.add(vehicles.get(i).getName());
-			}
-		}
-	}
-	
-	public void filterVehicleTypes() {
-		for (int i = 0; i < vehicles.size(); i++) {
-			boolean c = false;
-			for (int j = 0; j < selectedVehicleTypes.size(); j++) {
-				if(vehicles.get(i).getType().equals(selectedVehicleTypes.get(j))) {
-					c = true;
-				}
-			}
-			if (c == false ) {
-				selectedVehicleTypes.add(vehicles.get(i).getType());
-			}
-		}
-	}
-	
-	public void filterClasses() {
-		for (int i = 0; i < vehicles.size(); i++) {
-			boolean c = false;
-			for (int j = 0; j < selectedClasses.size(); j++) {
-				if(vehicles.get(i).getVehicleClass().equals(selectedClasses.get(j))) {
-					c = true;
-				}
-			}
-			if (c == false ) {
-				selectedClasses.add(vehicles.get(i).getVehicleClass());
-			}
-		}
-	}
-	
-	public void filterGearType() {
-		for (int i = 0; i < vehicles.size(); i++) {
-			boolean c = false;
-			for (int j = 0; j < selectedGearTypes.size(); j++) {
-				if(vehicles.get(i).getGearType().equals(selectedGearTypes.get(j))) {
-					c = true;
-				}
-			}
-			if (c == false ) {
-				selectedGearTypes.add(vehicles.get(i).getGearType());
-			}
-		}
-	}
-	
-	public void filterFuelType() {
-		for (int i = 0; i < vehicles.size(); i++) {
-			boolean c = false;
-			for (int j = 0; j < selectedFuelTypes.size(); j++) {
-				if(vehicles.get(i).getFuelType().equals(selectedFuelTypes.get(j))) {
-					c = true;
-				}
-			}
-			if (c == false ) {
-				selectedFuelTypes.add(vehicles.get(i).getFuelType());
-			}
-		} 
-	}*/
 
 	public ArrayList<String> getNameSelections() {
 		return nameSelections;
@@ -375,46 +218,6 @@ public class OfficeBean implements Serializable{
 
 	public void setNameSelections(ArrayList<String> nameSelections) {
 		this.nameSelections = nameSelections;
-	}
-
-	public ArrayList<String> getClassSelections() {
-		return classSelections;
-	}
-
-	public void setClassSelections(ArrayList<String> classSelections) {
-		this.classSelections = classSelections;
-	}
-
-	public ArrayList<String> getVehicleTypeSelections() {
-		return vehicleTypeSelections;
-	}
-
-	public void setVehicleTypeSelections(ArrayList<String> vehicleTypeSelections) {
-		this.vehicleTypeSelections = vehicleTypeSelections;
-	}
-
-	public ArrayList<String> getGearTypeSelections() {
-		return gearTypeSelections;
-	}
-
-	public void setGearTypeSelections(ArrayList<String> gearTypeSelections) {
-		this.gearTypeSelections = gearTypeSelections;
-	}
-
-	public ArrayList<String> getFuelTypeSelections() {
-		return fuelTypeSelections;
-	}
-
-	public void setFuelTypeSelections(ArrayList<String> fuelTypeSelections) {
-		this.fuelTypeSelections = fuelTypeSelections;
-	}
-
-	public String getSelectedPriceRange() {
-		return selectedPriceRange;
-	}
-
-	public void setSelectedPriceRange(String selectedPriceRange) {
-		this.selectedPriceRange = selectedPriceRange;
 	}
 
 	public ArrayList<String> getSelectedNames() {
@@ -447,22 +250,6 @@ public class OfficeBean implements Serializable{
 
 	public void setSelectedcountry(ArrayList<String> selectedcountry) {
 		this.selectedcountry = selectedcountry;
-	}
-
-	public ArrayList<String> getSelectedGearTypes() {
-		return selectedGearTypes;
-	}
-
-	public void setSelectedGearTypes(ArrayList<String> selectedGearTypes) {
-		this.selectedGearTypes = selectedGearTypes;
-	}
-
-	public ArrayList<String> getSelectedFuelTypes() {
-		return selectedFuelTypes;
-	}
-
-	public void setSelectedFuelTypes(ArrayList<String> selectedFuelTypes) {
-		this.selectedFuelTypes = selectedFuelTypes;
 	}
 
 	public ArrayList<Office> getOffices() {
@@ -500,9 +287,6 @@ public class OfficeBean implements Serializable{
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
-	
-
 
 //getter setters
 
